e281e5d75b01a2f02f6106ed293a36eb527d9a2e
Daniel <134326067+Daniel-P-Lima@users.noreply.github.com>
2025-05-28 23:07:49 -0300

    Update app.js

--- macrofinds-backend/app.js
+++ macrofinds-backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host : "localhost",
     user : "root",
-    password : "Oportunity23#",
+    password : "",
     database : "macrofinds"
 });
 
