478ecccd66fc1959f9cd293a1aa668db37911ca6
Daniel-P-Lima <limadanielpereira@gmail.com>
2025-05-30 20:01:51 -0300

    arruma codigo main

--- macrofinds-frontend/src/pages/UserData/UserData.jsx
+++ macrofinds-frontend/src/pages/UserData/UserData.jsx
@@ -1,92 +1,121 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from "../../assets/Navbar/Navbar"
-import "./UserData.css"
 
 const UserData = () => {
     const [userData, setUserData] = useState({
         peso: '',
         altura: '',
-        biotipo: '',
-        intensidade: ''
+        sexo: '',
+        intensidade: '',
+        idade: ''
     });
 
-    /*
-        Busca de dados para TMB
-    */
+    const [tmb, setTmb] = useState('');
+
     useEffect(() => {
         fetch('http://localhost:3000/usuario/1')
         .then(res => res.json())
         .then(data => {
             setUserData({
-            peso: data.peso_usuario || '',
-            altura: data.altura_usuario || '',
-            biotipo: data.biotipo_usuario || '',
-            intensidade: data.tipo_atividade_fisica || ''
+                peso: data.peso_usuario || '',
+                altura: data.altura_usuario || '',
+                sexo: data.sexo_usuario || '',
+                intensidade: data.tipo_atividade_fisica || '',
+                idade: data.idade_usuario || ''
+            });
         });
-    })
-
     }, []);
 
-  return (
-    <div className='dieta-main'>
-        <div className='left-column column'>
-            <Navbar />
-        </div>
+    const calcularTmb = () => {
+        
+        const peso = parseFloat(userData.peso);
+        const altura = parseFloat(userData.altura);
+        const idade = parseFloat(userData.idade);
+        let resultado = 0;
+
+        console.log("peso", peso);
+        console.log("altura", altura);
+        console.log("idade", idade);
+
+
+
+
+        if (userData.sexo === 'Masculino') {
+            resultado = 66 + (13.7 * peso) + (5 * altura) - (6.8 * idade);
+        } else if (userData.sexo === 'Feminino') {
+            resultado = 655 + (9.6 * peso) + (1.7 * altura) - (4.7 * idade);
+        }
+
+        let fator = 1;
+        if (userData.intensidade === 'N') fator = 1.2;
+        else if (userData.intensidade === 'M') fator = 1.35;
+        else if (userData.intensidade === 'I') fator = 1.45;
+
+        const tmbFinal = resultado * fator;
+
+        setTmb(tmbFinal.toFixed(2));
+    };
+
+    return (
         <div className='main'>
             <div className='header-tmb'>
                 <header>
-                    <h2>Calculadora de TMB (Taxa de Metabolismo Basal)</h2>
+                    <h1>Calculadora TMB (Taxa de Metabolismo Basal)</h1>
                 </header>
             </div>
-        <div className='container'>
-            <div className='main-container'>
-                <h2>Sua TMB é...</h2>
-                <h1 className='tmb-val'>2.3555cal/dia<img className='heat-img' src='/heat.png' alt='heat'/></h1>
-                <p>Baseado no seu peso, altura, biotipo e intensidade de atividade física</p>
-                <div className='input-container'>
-                <form className='input-container-form'>
-                    <label>Peso</label>
-                    <input
-                    type="text"
-                    value={userData.peso}
-                    onChange={e => setUserData({ ...userData, peso: e.target.value })}
-                    />
+            <div className='container'>
+                <div className='main-container'>
+                    <h2>Sua TMB é...</h2>
+                    <h1 className='tmb-val'>{tmb ? `${tmb} cal/dia` : '---'}<img className='heat-img' src='/heat.png' /></h1>
+                    <p>Baseado no seu peso, altura, sexo, idade e intensidade de atividade física</p>
+                    <div className='input-container'>
+                        <form className='input-container-form' onSubmit={e => e.preventDefault()}>
+                            <label>Peso</label>
+                            <input
+                                type="text"
+                                value={userData.peso}
+                                onChange={e => setUserData({ ...userData, peso: e.target.value })}
+                            />
 
-                    <label>Altura</label>
-                    <input
-                    type="text"
-                    value={userData.altura}
-                    onChange={e => setUserData({ ...userData, altura: e.target.value })}
-                    />
+                            <label>Altura</label>
+                            <input
+                                type="text"
+                                value={userData.altura}
+                                onChange={e => setUserData({ ...userData, altura: e.target.value })}
+                            />
 
-                    <label>Biotipo</label>
-                    <select
-                    value={userData.biotipo}
-                    onChange={e => setUserData({ ...userData, biotipo: e.target.value })}
-                    >
-                    <option value="Ectomorfo">Ectomorfo</option>
-                    <option value="Endomorfo">Endomorfo</option>
-                    <option value="Mesomorfo">Mesomorfo</option>
-                    </select>
+                            <label>Sexo</label>
+                            <select
+                                value={userData.sexo}
+                                onChange={e => setUserData({ ...userData, sexo: e.target.value })}
+                            >
+                                <option value="Masculino">Masculino</option>
+                                <option value="Feminino">Feminino</option>
+                            </select>
 
-                    <label>Intensidade</label>
-                    <select
-                    value={userData.intensidade}
-                    onChange={e => setUserData({ ...userData, intensidade: e.target.value })}
-                    >
-                    <option value="N">Normal</option>
-                    <option value="M">Moderada</option>
-                    <option value="I">Intensa</option>
-                    </select>
+                            <label>Idade</label>
+                            <input
+                                type="text"
+                                value={userData.idade}
+                                onChange={e => setUserData({ ...userData, idade: e.target.value })}
+                            />
 
-                    <button type="button" className='button-calc'>Calcular</button>
-                </form>
+                            <label>Intensidade</label>
+                            <select
+                                value={userData.intensidade}
+                                onChange={e => setUserData({ ...userData, intensidade: e.target.value })}
+                            >
+                                <option value="N">Normal</option>
+                                <option value="M">Moderada</option>
+                                <option value="I">Intensa</option>
+                            </select>
+
+                            <button type="button" className='button-calc' onClick={calcularTmb}>Calcular</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default UserData;
